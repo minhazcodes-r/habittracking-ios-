ad41bc2d651e187a86579462050b7b79d19e59e6
MinhazCodes-R <minhazur.rakin101@gmail.com>
2026-04-29 17:04:05 -0400

    Initial Commit

---
+++ miniHabits/miniHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniHabitsApp.swift
+//  miniHabits
+//
+//  Created by Minhaz Rakin on 2026-04-29.
+//
+
+import SwiftUI
+
+@main
+struct miniHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
